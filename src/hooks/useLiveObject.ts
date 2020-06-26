a7a1d55eddba5c003c62b2fa64d1a3d04aa48aac
Aidar Lukmanov <aidar@ekoapp.com>
2020-06-26 10:52:16 +0700

    hook to simplify live object usage

---
+++ src/hooks/useLiveObject.ts
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+// TODO add errors handling
+const useLiveObject = (createLiveObject, defaultData) => {
+  const [data, setData] = useState(defaultData);
+  useEffect(() => {
+    const liveObject = createLiveObject();
+    liveObject.on('dataUpdated', setData);
+    return () => liveObject.dispose();
+  }, []);
+
+  return data;
+};
+
+export default useLiveObject;
