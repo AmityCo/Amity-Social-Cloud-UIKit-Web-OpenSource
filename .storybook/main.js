0bbcf41ce3174b32a2282ceed29abd209fe4e366
Aidar Lukmanov <aidar@ekoapp.com>
2020-07-31 14:50:34 +0700

    Fix storybook static export styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,33 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ['../src/**/*.stories.tsx'],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
   addons: [],
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
     });
 
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
+      loader: require.resolve('babel-loader'),
       options: {
-        presets: [["react-app", { flow: false, typescript: true }]]
-      }
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
     });
-    config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    config.plugins.push(
+      // fix static storybook styling
+      new webpack.DefinePlugin({
+        SC_DISABLE_SPEEDY: true,
+      }),
+    );
 
     return config;
-  }
+  },
 };
