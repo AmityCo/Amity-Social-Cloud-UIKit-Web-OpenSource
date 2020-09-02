e7eb993c93699586428e921bb467cf716f0e0ea3
cpv123 <cp.vibert@gmail.com>
2020-09-02 16:00:32 +0700

    Add theme setup and Upstra logo to storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+import logo from './upstra-logo.svg';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Upstra',
+  brandImage: logo,
+  brandUrl: 'https://upstra.co/',
+});
