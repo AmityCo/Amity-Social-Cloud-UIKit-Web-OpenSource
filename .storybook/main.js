271cc7c2fed689858540aa879ba2a326701692e7
chaiwattsw <tongbboy29@hotmail.com>
2020-09-03 12:19:17 +0700

    add storybook actions addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: ['@storybook/addon-storysource', '@storybook/addon-controls'],
+  addons: ['@storybook/addon-storysource', '@storybook/addon-controls', '@storybook/addon-actions'],
   webpackFinal: async (config) => {
     config.resolve.extensions.push('.js');
 
