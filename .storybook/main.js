2dba83d8968400ba6289741f148a2b0a09a675c3
chaiwattsw <tongbboy29@hotmail.com>
2020-09-08 11:12:12 +0700

    add storybook/addon-controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ const mainWebpack = require('../webpack.config.js');
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: ['@storybook/addon-storysource'],
+  addons: ['@storybook/addon-storysource', '@storybook/addon-controls'],
   webpackFinal: async config => {
     config.plugins.push(
       // fix static storybook styling
