3b61836a4bab63309378f39056a22109ff64b605
cpv123 <cp.vibert@gmail.com>
2020-09-09 14:57:03 +0700

    Fix importing of external CSS

--- src/components/UiKitProvider/GlobalStyle.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-import 'react-toggle/style.css';
-
-const GlobalStyle = createGlobalStyle`
-@import url("https://rsms.me/inter/inter.css"); 
-`;
-
-export default GlobalStyle;
