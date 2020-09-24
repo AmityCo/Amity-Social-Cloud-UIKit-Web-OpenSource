2aaa5357a920947fac526b2a65443c6adaa70485
Julien Barbay <julien.barbay@gmail.com>
2020-09-24 16:41:27 +0700

    refactoring of file icons

--- src/icons/Icon.stories.js
+++ src/icons/Icon.stories.js
@@ -7,3 +7,34 @@ export default {
 };
 
 export const User = () => <Icons.User />;
+
+export const Files = () => (
+  <div
+    style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(5, 1fr)',
+      gridAutoRows: '4em',
+      gridGap: '1em',
+    }}
+  >
+    <Icons.AudioFile />
+    <Icons.AviFile />
+    <Icons.CsvFile />
+    <Icons.DefaultFile />
+    <Icons.DocFile />
+    <Icons.ExeFile />
+    <Icons.HtmlFile />
+    <Icons.ImgFile />
+    <Icons.MovFile />
+    <Icons.Mp3File />
+    <Icons.Mp4File />
+    <Icons.MpegFile />
+    <Icons.PdfFile />
+    <Icons.PptFile />
+    <Icons.PpxFile />
+    <Icons.RarFile />
+    <Icons.TxtFile />
+    <Icons.XlsFile />
+    <Icons.ZipFile />
+  </div>
+);
--- src/icons/index.js
+++ src/icons/index.js
@@ -1,1 +1,22 @@
 export { default as User } from './User';
+
+export { default as AudioFile } from './files/Audio';
+export { default as AviFile } from './files/Avi';
+export { default as CsvFile } from './files/Csv';
+export { default as DefaultFile } from './files/Default';
+export { default as DocFile } from './files/Doc';
+export { default as ExeFile } from './files/Exe';
+export { default as File } from './files/File';
+export { default as HtmlFile } from './files/Html';
+export { default as ImgFile } from './files/Img';
+export { default as MovFile } from './files/Mov';
+export { default as Mp3File } from './files/Mp3';
+export { default as Mp4File } from './files/Mp4';
+export { default as MpegFile } from './files/Mpeg';
+export { default as PdfFile } from './files/Pdf';
+export { default as PptFile } from './files/Ppt';
+export { default as PpxFile } from './files/Ppx';
+export { default as RarFile } from './files/Rar';
+export { default as TxtFile } from './files/Txt';
+export { default as XlsFile } from './files/Xls';
+export { default as ZipFile } from './files/Zip';
