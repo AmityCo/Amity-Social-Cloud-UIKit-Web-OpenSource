8da8e3db170ac7ec527a61a5ebc7d3688134cae5
Chaiwat Trisuwan <tongbboy29@hotmail.com>
2020-09-29 18:19:43 +0000

    [Feature] UKT-507: small components to stories (tab component)

--- src/core/components/Tabs/index.js
+++ src/core/components/Tabs/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { customizableComponent } from '~/core/hocs/customization';
-import Tab from '~/core/components/Tab';
 
-import { TabsContainer } from './styles';
+import { TabsContainer, TabsList, TabItem, TabButton } from './styles';
 
 const Tabs = ({ className, tabs, activeTab, onChange }) => (
   <TabsContainer className={className}>
-    {tabs.map(tab => (
-      <Tab key={tab} active={tab === activeTab} onClick={() => onChange(tab)}>
-        {tab}
-      </Tab>
-    ))}
+    <TabsList>
+      {Object.entries(tabs).map(([key, val]) => (
+        <TabItem key={key}>
+          <TabButton className={key === activeTab ? 'active' : ''} onClick={() => onChange(key)}>
+            {val}
+          </TabButton>
+        </TabItem>
+      ))}
+    </TabsList>
   </TabsContainer>
 );
 
--- src/core/components/Tabs/styles.js
+++ src/core/components/Tabs/styles.js
@@ -1,6 +1,47 @@
 import styled from 'styled-components';
 
-export const TabsContainer = styled.div`
-  border-bottom: 1px solid #edeef2;
+export const TabsContainer = styled.nav`
+  background: #ffffff;
+  border-radius: 4px 4px 0 0;
+  border: 1px solid #edeef2;
+`;
+
+export const TabsList = styled.ul`
+  margin: 0;
   padding: 0 16px;
+  list-style-type: none;
+`;
+
+export const TabItem = styled.li`
+  display: inline-block;
+`;
+
+export const TabButton = styled.button`
+  padding: 0.75em;
+  margin-right: 0.5em;
+  background-color: #ffffff;
+  border: none;
+  border-bottom: 2px solid transparent;
+  cursor: pointer;
+  outline: none;
+  color: #abaeba;
+  ${({ theme }) => theme.typography.bodyBold}
+  text-align: center;
+
+  &:hover {
+    color: #818698;
+  }
+
+  &.active {
+    ${({ theme }) => `
+      border-bottom: 2px solid ${theme.palette.primary.main};
+      color: ${theme.palette.primary.main};
+    `}
+  }
+
+  &:disabled {
+    color: #abaeba;
+  }
+
+  transition: border-color 0.3s;
 `;
