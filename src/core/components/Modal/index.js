efaae35965956ed86814b8880830eea84ed14c58
Serhii Ivanenko <Serhii@amity.co>
2020-12-09 23:15:09 +0200

    UKT-1150 - fixed scroll on background for modal

--- src/core/components/Modal/index.js
+++ src/core/components/Modal/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import useElement from '~/core/hooks/useElement';
 import ConditionalRender from '~/core/components/ConditionalRender';
 import { Overlay, ModalWindow, Header, Content, Footer, CloseIcon } from './styles';
 
-const Modal = ({ className, onOverlayClick, onCancel, title, footer, clean, children }) => (
-  <Overlay onClick={onOverlayClick}>
-    <ModalWindow className={className}>
-      <ConditionalRender condition={title || onCancel}>
-        <Header clean={clean}>
-          {title}
-          <ConditionalRender condition={onCancel}>
-            <CloseIcon onClick={onCancel} />
-          </ConditionalRender>
-        </Header>
-      </ConditionalRender>
-      <Content isText={typeof children === 'string'}>{children}</Content>
-      <ConditionalRender condition={footer}>
-        <Footer clean={clean}>{footer}</Footer>
-      </ConditionalRender>
-    </ModalWindow>
-  </Overlay>
-);
+const Modal = ({ className, onOverlayClick, onCancel, title, footer, clean, children }) => {
+  const [modalRef, modalElement] = useElement();
+  // auto focus to prevent scroll on background (when focus kept on trigger button)
+  useEffect(() => modalElement && modalElement.focus(), [modalElement]);
+
+  return (
+    <Overlay onClick={onOverlayClick}>
+      <ModalWindow className={className} ref={modalRef} tabIndex={0}>
+        <ConditionalRender condition={title || onCancel}>
+          <Header clean={clean}>
+            {title}
+            <ConditionalRender condition={onCancel}>
+              <CloseIcon onClick={onCancel} />
+            </ConditionalRender>
+          </Header>
+        </ConditionalRender>
+        <Content isText={typeof children === 'string'}>{children}</Content>
+        <ConditionalRender condition={footer}>
+          <Footer clean={clean}>{footer}</Footer>
+        </ConditionalRender>
+      </ModalWindow>
+    </Overlay>
+  );
+};
 
 export default Modal;
