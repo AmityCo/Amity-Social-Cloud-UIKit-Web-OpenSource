0d220ed068624395c6ed60e4e5cc6ab4f2f6f689
Valeriy Malyshev <valeriy@amity.co>
2021-02-04 19:06:54 +0300

    UP-1296 - wrap social app with NavigationProvider

--- src/social/pages/Application/index.js
+++ src/social/pages/Application/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import styled from 'styled-components';
 
 import { PageTypes } from '~/social/constants';
@@ -15,6 +14,7 @@ import CommunityFeedPage from '~/social/pages/CommunityFeed';
 import UserFeedPage from '~/social/pages/UserFeed';
 import CategoryCommunitiesPage from '~/social/pages/CategoryCommunities';
 import CommunityEditPage from '~/social/pages/CommunityEdit';
+import NavigationProvider, { useNavigation } from '~/social/providers/NavigationProvider';
 
 const ApplicationContainer = styled.div`
   height: 100%;
@@ -25,7 +25,41 @@ const StyledCommunitySideMenu = styled(CommunitySideMenu)`
   min-height: 100%;
 `;
 
-const Community = ({
+const Community = () => {
+  const { page } = useNavigation();
+
+  return (
+    <ApplicationContainer>
+      <MainLayout aside={<StyledCommunitySideMenu activeCommunity={page.communityId} />}>
+        <ConditionalRender condition={page.type === PageTypes.Explore}>
+          <ExplorePage />
+        </ConditionalRender>
+
+        <ConditionalRender condition={page.type === PageTypes.NewsFeed}>
+          <NewsFeedPage />
+        </ConditionalRender>
+
+        <ConditionalRender condition={page.type === PageTypes.CommunityFeed}>
+          <CommunityFeedPage communityId={page.communityId} />
+        </ConditionalRender>
+
+        <ConditionalRender condition={page.type === PageTypes.CommunityEdit}>
+          <CommunityEditPage communityId={page.communityId} />
+        </ConditionalRender>
+
+        <ConditionalRender condition={page.type === PageTypes.UserFeed}>
+          <UserFeedPage userId={page.userId} />
+        </ConditionalRender>
+
+        <ConditionalRender condition={page.type === PageTypes.Category}>
+          <CategoryCommunitiesPage categoryId={page.categoryId} />
+        </ConditionalRender>
+      </MainLayout>
+    </ApplicationContainer>
+  );
+};
+
+export default ({
   onChangePage,
   onClickCommunity,
   onCommunityCreated,
@@ -34,186 +68,17 @@ const Community = ({
   onEditUser,
   onMessageUser,
   onEditCommunity,
-}) => {
-  const [page, setPage] = useState({ type: PageTypes.NewsFeed });
-
-  const handleChangePage = type => {
-    if (onChangePage) return onChangePage({ type });
-
-    setPage({ type });
-  };
-
-  const handleClickCommunity = communityId => {
-    const next = {
-      type: PageTypes.CommunityFeed,
-      communityId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onClickCommunity) return onClickCommunity(communityId);
-
-    console.log('handleClickCommunity', { communityId });
-    setPage(next);
-  };
-
-  const handleCommunityCreated = communityId => {
-    const next = {
-      type: PageTypes.CommunityFeed,
-      communityId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onCommunityCreated) return onCommunityCreated(communityId);
-
-    console.log('handleCommunityCreated', { communityId });
-    setPage(next);
-  };
-
-  const handleClickCategory = categoryId => {
-    const next = {
-      type: PageTypes.Category,
-      categoryId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onClickCategory) return onClickCategory(categoryId);
-
-    console.log('handleClickCategory', { categoryId });
-    setPage(next);
-  };
-
-  const handleClickUser = userId => {
-    const next = {
-      type: PageTypes.UserFeed,
-      userId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onClickUser) return onClickUser(userId);
-
-    console.log('handleClickUser', { userId });
-    setPage(next);
-  };
-
-  const handleEditUser = userId => {
-    const next = {
-      type: 'userSettings',
-      userId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onEditUser) return onEditUser(userId);
-
-    console.log('handleEditUser', { userId });
-  };
-
-  const handleEditCommunity = communityId => {
-    const next = {
-      type: PageTypes.CommunityEdit,
-      communityId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onEditCommunity) return onEditCommunity(communityId);
-
-    console.log('handleEditCommunity', { communityId });
-    setPage(next);
-  };
-
-  const handleMessageUser = userId => {
-    const next = {
-      type: 'conversation',
-      userId,
-    };
-
-    if (onChangePage) return onChangePage(next);
-    if (onMessageUser) return onMessageUser(userId);
-
-    console.log('handleMessageUser', { userId });
-  };
-
-  return (
-    <ApplicationContainer>
-      <MainLayout
-        aside={
-          <StyledCommunitySideMenu
-            newsFeedActive={page.type === PageTypes.NewsFeed}
-            onClickNewsFeed={() => handleChangePage(PageTypes.NewsFeed)}
-            exploreActive={page.type === PageTypes.Explore}
-            onClickExplore={() => handleChangePage(PageTypes.Explore)}
-            activeCommunity={page.communityId}
-            onClickCommunity={handleClickCommunity}
-          />
-        }
-      >
-        <ConditionalRender condition={page.type === PageTypes.Explore}>
-          <ExplorePage
-            onClickCommunity={handleClickCommunity}
-            onCommunityCreated={handleCommunityCreated}
-            onClickUser={handleClickUser}
-            onClickCategory={handleClickCategory}
-          />
-        </ConditionalRender>
-
-        <ConditionalRender condition={page.type === PageTypes.NewsFeed}>
-          <NewsFeedPage
-            onClickCommunity={handleClickCommunity}
-            onClickUser={handleClickUser}
-            onClickCategory={handleClickCategory}
-          />
-        </ConditionalRender>
-
-        <ConditionalRender condition={page.type === PageTypes.CommunityFeed}>
-          <CommunityFeedPage
-            communityId={page.communityId}
-            onClickCommunity={handleClickCommunity}
-            onClickUser={handleClickUser}
-            onClickCategory={handleClickCategory}
-            onEditCommunity={handleEditCommunity}
-          />
-        </ConditionalRender>
-
-        <ConditionalRender condition={page.type === PageTypes.CommunityEdit}>
-          <CommunityEditPage
-            communityId={page.communityId}
-            onReturnToCommunity={handleClickCommunity}
-            onClickUser={handleClickUser}
-            onCommunityClosed={() => handleChangePage(PageTypes.Explore)}
-          />
-        </ConditionalRender>
-
-        <ConditionalRender condition={page.type === PageTypes.UserFeed}>
-          <UserFeedPage
-            userId={page.userId}
-            onClickCommunity={handleClickCommunity}
-            onClickUser={handleClickUser}
-            onClickCategory={handleClickCategory}
-            onEditUser={handleEditUser}
-            onMessageUser={handleMessageUser}
-          />
-        </ConditionalRender>
-
-        <ConditionalRender condition={page.type === PageTypes.Category}>
-          <CategoryCommunitiesPage
-            categoryId={page.categoryId}
-            onBack={() => handleChangePage(PageTypes.Explore)}
-            onClickCommunity={handleClickCommunity}
-          />
-        </ConditionalRender>
-      </MainLayout>
-    </ApplicationContainer>
-  );
-};
-
-Community.propTypes = {
-  onChangePage: PropTypes.func,
-  onClickCommunity: PropTypes.func,
-  onCommunityCreated: PropTypes.func,
-  onClickCategory: PropTypes.func,
-  onClickUser: PropTypes.func,
-  onEditUser: PropTypes.func,
-  onMessageUser: PropTypes.func,
-  onEditCommunity: PropTypes.func,
-};
-
-export default Community;
+}) => (
+  <NavigationProvider
+    onChangePage={onChangePage}
+    onClickCategory={onClickCategory}
+    onClickCommunity={onClickCommunity}
+    onClickUser={onClickUser}
+    onCommunityCreated={onCommunityCreated}
+    onEditCommunity={onEditCommunity}
+    onEditUser={onEditUser}
+    onMessageUser={onMessageUser}
+  >
+    <Community />
+  </NavigationProvider>
+);
