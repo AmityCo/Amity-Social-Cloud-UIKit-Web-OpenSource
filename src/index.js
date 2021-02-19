28aaae3c661e2c2ab9c036adfcbe4eed812b1d80
Valeriy Malyshev <valeriy@amity.co>
2021-02-19 10:02:12 +0300

    export required staff

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ export { default as UpstraUiKitFeed } from '~/social/components/Feed';
 export { default as UpstraUiKitSocial } from '~/social/pages/Application';
 export { default as UpstraUiKitPostRendererProvider } from '~/social/providers/PostRendererProvider';
 
-// import UpstraAvatar from './components/Avatar';
+export { default as useUpstraUser } from '~/core/hooks/useUser';
+export { useNavigation as useUpstraNavigation } from '~/social/providers/NavigationProvider';
+
+export { default as UpstraAvatar } from '~/core/components/Avatar';
+export { PostContainer as UpstraPostContainer } from '~/social/components/post/Post/styles';
+export { default as UpstraPostEngagementBar } from '~/social/components/EngagementBar';
+export { default as UpstraExpandableText } from '~/social/components/Comment/CommentText';
+
 // import UpstraComment from './components/Comment';
 // import UpstraCommentComposeBar from './components/CommentComposeBar';
 // import UpstraCommentLikeButton from './components/CommentLikeButton';
 // import UpstraCommunity from './components/Community';
 // import UpstraCommunityItem from './components/CommunityItem';
 // import UpstraEmptyFeed from './components/EmptyFeed';
-// import UpstraEngagementBar from './components/EngagementBar';
 // import UpstraFiles from './components/Files';
 // import UpstraImageGallery from './components/ImageGallery';
 // import UpstraImages from './components/Images';
