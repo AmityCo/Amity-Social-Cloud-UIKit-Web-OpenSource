510aca7000769ae12a8cba9ec9b97b5fbe8dce2f
Valeriy Malyshev <valeriy@amity.co>
2021-02-22 18:08:46 +0300

    fix: the user search

--- src/core/hooks/useUserQuery.js
+++ src/core/hooks/useUserQuery.js
@@ -2,7 +2,7 @@ import { UserRepository } from 'eko-sdk';
 
 import useLiveCollection from '~/core/hooks/useLiveCollection';
 
-const MINIMUM_STRING_LENGTH_TO_TRIGGER_QUERY = 2;
+const MINIMUM_STRING_LENGTH_TO_TRIGGER_QUERY = 1;
 
 const userRepo = new UserRepository();
 
@@ -10,7 +10,7 @@ const useUserQuery = term => {
   const [users, hasMore, loadMore] = useLiveCollection(
     () => userRepo.searchUserByDisplayName(term),
     [term],
-    () => term && term.length >= MINIMUM_STRING_LENGTH_TO_TRIGGER_QUERY,
+    () => !term || term.length < MINIMUM_STRING_LENGTH_TO_TRIGGER_QUERY,
   );
 
   return [users, hasMore, loadMore];
