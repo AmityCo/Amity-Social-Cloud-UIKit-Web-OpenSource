283915607f3024748cb3d28e77cdf61ef59d7a22
Predjo <predovan.nikola@gmail.com>
2022-07-18 19:18:52 +0200

    fix: Fix page types

--- src/social/constants.js
+++ src/social/constants.js
@@ -1,11 +1,11 @@
 export const PageTypes = {
   Explore: 'explore',
   Category: 'category',
-  NewsFeed: 'newsfeed',
-  UserFeed: 'userfeed',
-  CommunityFeed: 'communityfeed',
-  CommunityEdit: 'communityedit',
-  UserEdit: 'useredit',
+  NewsFeed: 'newsFeed',
+  UserFeed: 'userFeed',
+  CommunityFeed: 'communityFeed',
+  CommunityEdit: 'communityEdit',
+  UserEdit: 'userEdit',
 };
 
 export const MemberRoles = Object.freeze({
