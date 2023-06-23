b68b0c25f04dc353c8825416391b9dd903dbced1
hectorcymbiotika <hector@cymbiotika.com>
2023-06-23 13:49:51 -0700

    updates to custom header and sidebar responsiveness.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,22 @@ import { FluidControl, UiKitDecorator } from './decorators';
 
 const decorators = [FluidControl.decorator, UiKitDecorator.decorator];
 
+// Get HTML head element
+const head = document.getElementsByTagName('HEAD')[0];
+
+// Create new link Element
+const link = document.createElement('link');
+
+// set the attributes for link element
+link.rel = 'stylesheet';
+
+link.type = 'text/css';
+
+link.href = 'https://use.typekit.net/zzt4gto.css';
+
+// Append link element to HTML head
+head.appendChild(link);
+
 const parameters = {
   options: {
     storySort: {
