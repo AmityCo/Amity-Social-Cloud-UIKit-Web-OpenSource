e400e5849eb9d5247c34686cf86e8f6d4049a34e
hectorcymbiotika <hector@cymbiotika.com>
2023-06-30 12:02:22 -0700

    Arise Tokens components and styling

--- src/social/components/AriseTokens/styles.js
+++ src/social/components/AriseTokens/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import MediaGalleryTabs from '~/core/components/Tabs/MediaGalleryTabs';
 
 export const AriseTokensContainer = styled.div`
-  padding: 1.25em 1em;
+  padding: 32px;
   background: ${({ theme }) => theme.palette.system.background};
   border: 1px solid ${({ theme }) => theme.palette.system.borders};
   border-radius: 0.25em;
