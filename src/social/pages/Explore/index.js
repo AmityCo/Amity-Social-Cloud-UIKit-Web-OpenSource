726f7cf1afc1536e2593c3f1c12a4576fc835469
hectorcymbiotika <hector@cymbiotika.com>
2023-07-31 10:24:35 -0700

    file management

--- src/social/pages/Explore/index.js
+++ src/social/pages/Explore/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import CategoriesCard from '~/social/components/category/CategoriesCard';
 import RecommendedList from '~/social/components/community/RecommendedList';
-import TrendingList from '~/social/components/community/TrendingList';
+// import TrendingList from '~/social/components/community/TrendingList';
 
 import MyCommunitiesMobile from '~/social/components/MyCommunitiesMobile';
 import FeaturedVideos from '~/social/components/FeaturedVideos';
