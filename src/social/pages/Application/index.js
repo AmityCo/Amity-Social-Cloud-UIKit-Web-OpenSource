402ec2dfb5df16afb8e817a47fc81f71efd49408
Mike Heitzmann <mikeheitzmann@gmail.com>
2023-08-14 18:56:25 -0500

    Vertical navigation so we can search players still

--- src/social/pages/Application/index.js
+++ src/social/pages/Application/index.js
@@ -15,6 +15,9 @@ import CategoryCommunitiesPage from '~/social/pages/CategoryCommunities';
 import CommunityEditPage from '~/social/pages/CommunityEdit';
 import ProfileSettings from '~/social/components/ProfileSettings';
 import { useNavigation } from '~/social/providers/NavigationProvider';
+import SideSectionCommunity from '~/social/components/SideSectionCommunity';
+import SideSectionMyCommunity from '~/social/components/SideSectionMyCommunity';
+import UiKitSocialSearch from '~/social/components/SocialSearch';
 
 const ApplicationContainer = styled.div`
   height: 100%;
@@ -25,34 +28,42 @@ const StyledCommunitySideMenu = styled(CommunitySideMenu)`
   min-height: 100%;
 `;
 
+const SocialSearch = styled(UiKitSocialSearch)`
+  background: ${({ theme }) => theme.palette.system.background};
+  padding: 0.5rem;
+`;
+
 const Community = () => {
-  const { page } = useNavigation();
+    const { page } = useNavigation();
 
-  return (
-    <ApplicationContainer>
-      <MainLayout aside={<StyledCommunitySideMenu activeCommunity={page.communityId} />}>
-        {page.type === PageTypes.Explore && <ExplorePage />}
+    return (
+        <ApplicationContainer>
+            <MainLayout>
+                <SocialSearch />
+                <SideSectionCommunity />
+                <SideSectionMyCommunity activeCommunity={page.communityId} showCreateButton />
+                {page.type === PageTypes.Explore && <ExplorePage />}
 
-        {page.type === PageTypes.NewsFeed && <NewsFeedPage />}
+                {page.type === PageTypes.NewsFeed && <NewsFeedPage />}
 
-        {page.type === PageTypes.CommunityFeed && (
-          <CommunityFeedPage communityId={page.communityId} isNewCommunity={page.isNewCommunity} />
-        )}
+                {page.type === PageTypes.CommunityFeed && (
+                    <CommunityFeedPage communityId={page.communityId} isNewCommunity={page.isNewCommunity} />
+                )}
 
-        {page.type === PageTypes.CommunityEdit && (
-          <CommunityEditPage communityId={page.communityId} tab={page.tab} />
-        )}
+                {page.type === PageTypes.CommunityEdit && (
+                    <CommunityEditPage communityId={page.communityId} tab={page.tab} />
+                )}
 
-        {page.type === PageTypes.Category && (
-          <CategoryCommunitiesPage categoryId={page.categoryId} />
-        )}
+                {page.type === PageTypes.Category && (
+                    <CategoryCommunitiesPage categoryId={page.categoryId} />
+                )}
 
-        {page.type === PageTypes.UserFeed && <UserFeedPage userId={page.userId} />}
+                {page.type === PageTypes.UserFeed && <UserFeedPage userId={page.userId} />}
 
-        {page.type === PageTypes.UserEdit && <ProfileSettings userId={page.userId} />}
-      </MainLayout>
-    </ApplicationContainer>
-  );
+                {page.type === PageTypes.UserEdit && <ProfileSettings userId={page.userId} />}
+            </MainLayout>
+        </ApplicationContainer>
+    );
 };
 
 export default Community;
