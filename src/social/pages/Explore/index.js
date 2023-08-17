40f9cfeb31586221407609daf07b650a519a107a
Mike Heitzmann <mikeheitzmann@gmail.com>
2023-08-17 14:36:52 -0500

    Can set the landing page to not be NewsFeed, prep for search page

--- src/social/pages/Explore/index.js
+++ src/social/pages/Explore/index.js
@@ -1,48 +1,64 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 
 import RecommendedList from '~/social/components/community/RecommendedList';
 import TrendingList from '~/social/components/community/TrendingList';
 import CategoriesCard from '~/social/components/category/CategoriesCard';
 
-import { PageContainer } from './styles';
+import {PageContainer} from './styles';
 import {BackButton, Header, Title} from "~/social/pages/CategoryCommunities/styles";
 import ArrowLeft from "~/icons/ArrowLeft";
 import {PageTypes} from "~/social/constants";
 import {useNavigation} from "~/social/providers/NavigationProvider";
+import PropTypes from "prop-types";
+import NewsFeed from "~/social/pages/NewsFeed";
 
-const ExplorePage = () => {
+const ExplorePage = forwardRef(
+    (
+        {
+            isLandingPage,
+        },
+        ref,
+    ) => {
 
-    const { onBack, lastPage } = useNavigation();
-    
-    return (
-        <PageContainer>
-            <Header>
-                <BackButton onClick={onBack}>
-                    <ArrowLeft height={14} />
-                </BackButton>
+        const {onBack, lastPage} = useNavigation();
 
-                {lastPage.type === PageTypes.NewsFeed && (
-                    <Title>{"Main Feed"}</Title>
+        return (
+            <PageContainer>
+                {!isLandingPage && (
+                    <Header>
+                        <BackButton onClick={onBack}>
+                            <ArrowLeft height={14}/>
+                        </BackButton>
+
+                        {lastPage.type === PageTypes.NewsFeed && (
+                            <Title>{"Main Feed"}</Title>
+                        )}
+
+                        {lastPage.type === PageTypes.Search && (
+                            <Title>{"Search & Communities"}</Title>
+                        )}
+
+                        {lastPage.type === PageTypes.UserFeed && (
+                            <Title>{"User Feed"}</Title>
+                        )}
+
+                        {lastPage.type === PageTypes.CommunityFeed && (
+                            <Title>{"Community Feed"}</Title>
+                        )}
+
+                    </Header>
                 )}
+                
+                <RecommendedList/>
+                <TrendingList/>
+                <CategoriesCard/>
+            </PageContainer>
+        );
+    },
+);
 
-                {lastPage.type === PageTypes.Search && (
-                    <Title>{"Search & Communities"}</Title>
-                )}
-
-                {lastPage.type === PageTypes.UserFeed && (
-                    <Title>{"User Feed"}</Title>
-                )}
-
-                {lastPage.type === PageTypes.CommunityFeed && (
-                    <Title>{"Community Feed"}</Title>
-                )}
-
-            </Header>
-            <RecommendedList/>
-            <TrendingList/>
-            <CategoriesCard/>
-        </PageContainer>
-    );
+ExplorePage.propTypes = {
+    isLandingPage: PropTypes.string
 };
 
 export default ExplorePage;
--- src/social/pages/NewsFeed/index.js
+++ src/social/pages/NewsFeed/index.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import { PostTargetType } from '@amityco/js-sdk';
+import React, {forwardRef} from 'react';
+import {PostTargetType} from '@amityco/js-sdk';
 
-import { PageTypes } from '~/social/constants';
+import {PageTypes} from '~/social/constants';
 import Feed from '~/social/components/Feed';
 
-import { useNavigation } from '~/social/providers/NavigationProvider';
+import {useNavigation} from '~/social/providers/NavigationProvider';
 
-import { Wrapper } from './styles';
+import {Wrapper} from './styles';
 import {BackButton, Header, Title} from "~/social/pages/CategoryCommunities/styles";
 import ArrowLeft from "~/icons/ArrowLeft";
+import PropTypes from "prop-types";
+import Community from "~/social/pages/Application";
 
-const NewsFeed = () => {
-  const { onBack, lastPage, onChangePage } = useNavigation();
+const NewsFeed = forwardRef(
+    (
+        {
+            isLandingPage
+        },
+        ref,
+    ) => {
+        const {onBack, lastPage, onChangePage} = useNavigation();
 
-  return (
-    <Wrapper data-qa-anchor="news-feed">
-        {lastPage.type === PageTypes.Search && (
-            <Header>
-                <BackButton onClick={onBack}>
-                    <ArrowLeft height={14} />
-                </BackButton>
-            <Title>{"Return to Search & Communities"}</Title>
-            </Header>
-        )}
-      <Feed
-        targetType={PostTargetType.GlobalFeed}
-        goToExplore={() => onChangePage(PageTypes.Explore)}
-        showPostCreator
-      />
-    </Wrapper>
-  );
+        return (
+            <Wrapper data-qa-anchor="news-feed">
+                {!isLandingPage && lastPage.type === PageTypes.Search && (
+                    <Header>
+                        <BackButton onClick={onBack}>
+                            <ArrowLeft height={14}/>
+                        </BackButton>
+                        <Title>{"Search & Communities"}</Title>
+                    </Header>
+                )}
+                <Feed
+                    targetType={PostTargetType.GlobalFeed}
+                    goToExplore={() => onChangePage(PageTypes.Explore)}
+                    showPostCreator
+                />
+            </Wrapper>
+        );
+    },
+);
+
+NewsFeed.propTypes = {
+    isLandingPage: PropTypes.string
 };
 
 export default NewsFeed;
