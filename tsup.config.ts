91f3b79c4ced8e0398a7f537113bcf30c125c624
Bonn <pittawatm@gmail.com>
2023-11-16 18:31:56 +0700

    chore: ASC-15847 - tsup bundler (#87)
    * feat: tsup
    
    * feat: tsup config
    
    * chore: js -> jsx
    
    * feat: update tsup.config.js
    
    * chore: update pnpm-lock.yaml

---
+++ tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup';
+import { replace } from 'esbuild-plugin-replace';
+import pkg from './package.json';
+
+export default defineConfig((options) => ({
+  entry: ['src/index.js'],
+  format: ['cjs', 'esm'],
+  dts: false,
+  sourcemap: options.sourcemap,
+  minify: options.minify,
+  clean: true,
+  splitting: true,
+  treeshake: true,
+  esbuildPlugins: [
+    replace({
+      include: /.(ts|js|jsx|tsx)$/,
+      __TEST__: 'false',
+      __VERSION__: `'${pkg.version}'`,
+      __DEV__:
+        '(typeof process !== "undefined" && process.env && process.env.NODE_ENV ? (process.env.NODE_ENV !== "production") : false)',
+    }),
+  ],
+}));
