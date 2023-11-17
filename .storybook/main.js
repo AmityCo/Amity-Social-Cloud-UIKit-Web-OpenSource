590e6604e59f6d75512c184ec77176b905a97948
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2023-11-17 15:47:14 +0700

    fix: ASC-15847 - add tsup and change storybook config (#95)
    * fix: add tsup and change storybook config
    
    * fix: remove amity ts sdk

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,12 @@ const mainWebpack = require('../webpack.config.js');
 const pkg = require('../package.json');
 
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: [
+    '../src/**/*.stories.js',
+    '../src/**/*.stories.jsx',
+    '../src/**/*.stories.ts',
+    '../src/**/*.stories.tsx',
+  ],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
   addons: [
     '@storybook/addon-actions',
@@ -11,23 +16,19 @@ module.exports = {
     '@storybook/addon-controls',
     '@storybook/addon-viewport',
     '@storybook/addon-toolbars',
-
     '@storybook/addon-a11y',
   ],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5'
+    builder: '@storybook/builder-webpack5',
   },
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.plugins.push(
       new DefinePlugin({
         SC_DISABLE_SPEEDY: true, // storybook
-      __VERSION__: `"${pkg.version}"`,
+        __VERSION__: `"${pkg.version}"`,
       }),
-      new EnvironmentPlugin(
-        Object.keys(process.env)
-          .filter(key => key.startsWith('STORYBOOK'))
-      ),
+      new EnvironmentPlugin(Object.keys(process.env).filter((key) => key.startsWith('STORYBOOK'))),
     );
 
     // Merge with resolve config from main webpack.
