deb28b121106eaf5166e719bb597db0a98493139
Pitchaya T <33589608+ptchayap@users.noreply.github.com>
2024-04-22 10:50:46 +0700

    fix: ASC-00000 - remove sheet loading state (#271)
    * fix: remove sheet in loading state
    
    * fix: remove wrong return

--- src/v4/chat/pages/AmityLiveChatPage/ChatContainer/ChatLoadingState.tsx
+++ src/v4/chat/pages/AmityLiveChatPage/ChatContainer/ChatLoadingState.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import Sheet from 'react-modal-sheet';
 import { Typography } from '~/v4/core/components';
 import Spinner from '~/social/components/Spinner';
 import HomeIndicator from '~/v4/chat/components/HomeIndicator';
 import styles from './styles.module.css';
-import clsx from 'clsx';
 
 const ChatLoadingState = ({ children }: { children?: React.ReactNode }) => {
   return (
     <>
-      <Sheet.Content>{children}</Sheet.Content>
+      <div className={styles.messageListPlaceholder}>{children}</div>
       <div className={styles.composeBarContainer}>
         <div className={styles.composeBar}>
           <div className={styles.textInputContainer}>
