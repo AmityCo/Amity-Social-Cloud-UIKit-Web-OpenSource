2349f87b383d2620651428bf5d61286e2fee3a6f
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-06-06 13:48:05 +0700

    fix: ASC-22947 - story can't add hyperlink (#373)
    * fix: hyperlink action condition
    
    * fix: hyperlink css
    
    * fix: remove unused
    
    * fix: hyperlink config condition
    
    * fix: css

--- src/v4/social/pages/DraftsPage/DraftsPage.tsx
+++ src/v4/social/pages/DraftsPage/DraftsPage.tsx
@@ -19,7 +19,7 @@ import { StoryRepository } from '@amityco/ts-sdk';
 
 import { HyperLinkConfig } from '~/v4/social/components';
 import { useConfirmContext } from '~/v4/core/providers/ConfirmProvider';
-import { useNotificationData, useNotifications } from '~/v4/core/providers/NotificationProvider';
+import { useNotifications } from '~/v4/core/providers/NotificationProvider';
 import { useNavigation } from '~/social/providers/NavigationProvider';
 import { PageTypes } from '~/social/constants';
 import { BaseVideoPreview } from '../../internal-components/VideoPreview';
@@ -51,7 +51,15 @@ const AmityDraftStoryPage = ({ targetId, targetType, mediaType }: AmityDraftStor
       data: { url: string; customText: string };
       type: Amity.StoryItemType;
     }[]
-  >([]);
+  >([
+    {
+      data: {
+        url: '',
+        customText: '',
+      },
+      type: 'hyperlink' as Amity.StoryItemType,
+    },
+  ]);
 
   const handleHyperLinkBottomSheetClose = () => {
     setIsHyperLinkBottomSheetOpen(false);
@@ -164,13 +172,13 @@ const AmityDraftStoryPage = ({ targetId, targetType, mediaType }: AmityDraftStor
   };
 
   const handleOnClickHyperLinkActionButton = () => {
-    if (hyperLink[0]?.data?.url === '') {
-      setIsHyperLinkBottomSheetOpen(true);
+    if (hyperLink[0]?.data?.url) {
+      notification.info({
+        content: formatMessage({ id: 'storyDraft.notification.hyperlink.error' }),
+      });
       return;
     }
-    notification.info({
-      content: formatMessage({ id: 'storyDraft.notification.hyperlink.error' }),
-    });
+    setIsHyperLinkBottomSheetOpen(true);
   };
 
   useEffect(() => {
