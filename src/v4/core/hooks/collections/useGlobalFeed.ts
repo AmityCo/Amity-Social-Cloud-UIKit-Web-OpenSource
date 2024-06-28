8196664a562f5d5f6249dbae01ee4a2d092e9909
ChayanitBm <chayanit@amity.co>
2024-06-28 13:50:21 +0700

    feat: sync api v5 (#455)

--- src/v4/core/hooks/collections/useGlobalFeed.ts
+++ src/v4/core/hooks/collections/useGlobalFeed.ts
@@ -4,13 +4,13 @@ import { useEffect, useMemo, useState } from 'react';
 const useGlobalFeed = () => {
   const [items, setItems] = useState<Amity.Post[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [queryToken, setQueryToken] = useState<number | null>(null);
+  const [queryToken, setQueryToken] = useState<string | null>(null);
   const [loadMoreHasBeenCalled, setLoadMoreHasBeenCalled] = useState(false);
 
   async function fetchMore() {
     try {
       setIsLoading(true);
-      const newPosts = await FeedRepository.queryGlobalFeed({
+      const newPosts = await FeedRepository.getCustomRankingGlobalFeed({
         limit: 10,
         queryToken: queryToken || undefined,
       });
