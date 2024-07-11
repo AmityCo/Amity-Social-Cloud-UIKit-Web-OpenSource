cf5bb43cb72e365fade6bfac73bc7794e30319db
Bonn <pittawat@amity.co>
2024-07-11 17:35:42 +0700

    chore: ASC-00000 - console story ad ui (#521)
    * chore: console story ad ui
    
    * chore: add TODO

--- src/v4/social/internal-components/StoryViewer/Renderers/storyAd.tsx
+++ src/v4/social/internal-components/StoryViewer/Renderers/storyAd.tsx
@@ -1,30 +1,13 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-import {
-  CustomRenderer,
-  Tester,
-} from '~/v4/social/internal-components/StoryViewer/Renderers/types';
-import { Button } from '~/v4/core/natives/Button';
+import { Tester, CustomRenderer } from './types';
 
-import styles from './storyAd.module.css';
-import clsx from 'clsx';
+import React, { useState, useEffect, useCallback } from 'react';
 
-import { StoryProgressBar } from '~/v4/social/elements/StoryProgressBar/StoryProgressBar';
-import { StoryAdInformation } from '../../StoryAdInformation/StoryAdInformation';
-import InfoCircle from '~/v4/icons/InfoCircle';
-import { AdsBadge } from '../../AdsBadge/AdsBadge';
-import { Avatar, Typography } from '~/v4/core/components/index';
-import Broadcast from '~/v4/icons/Broadcast';
 import useImage from '~/v4/core/hooks/useImage';
-import { PauseIcon, PlayIcon } from '~/icons/index';
-import { CloseButton } from '~/v4/social/elements/index';
+import { UIStoryAd } from '../../StoryAd/UIStoryAd';
 
 export const renderer: CustomRenderer = ({ story, action, config, onClose }) => {
-  const [loaded, setLoaded] = useState(false);
-  const [isAdvertisementInfoOpen, setIsAdvertisementInfoOpen] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
   const { loader } = config;
-  const imageRef = useRef<HTMLImageElement>(null);
-  const targetRef = useRef<HTMLDivElement>(null);
 
   const { ad, currentIndex, storiesCount, increaseIndex, pageId, dragEventTarget } = story;
 
@@ -43,21 +26,18 @@ export const renderer: CustomRenderer = ({ story, action, config, onClose }) => 
   const adImageFile = useImage({ fileId: ad?.image9_16?.fileId });
   const adImageUrl = adImageFile || ad?.image9_16?.fileUrl || '';
 
-  const imageLoaded = useCallback(() => {
-    setLoaded(true);
+  const onImageLoaded = useCallback(() => {
     if (isPaused) {
       setIsPaused(false);
     }
     action('play', true);
   }, [action, isPaused]);
 
-  const openAdvertisementInfo = () => {
+  const onAdvertisementInfoOpen = () => {
     action('pause', true);
-    setIsAdvertisementInfoOpen(true);
   };
-  const closeAdvertisementInfo = () => {
+  const onAdvertisementInfoClose = () => {
     action('play', true);
-    setIsAdvertisementInfoOpen(false);
   };
 
   const handleProgressComplete = () => {
@@ -90,86 +70,23 @@ export const renderer: CustomRenderer = ({ story, action, config, onClose }) => 
   }
 
   return (
-    <div className={styles.rendererContainer} ref={targetRef}>
-      <StoryProgressBar
-        pageId={pageId}
-        duration={5000}
-        currentIndex={currentIndex}
-        storiesCount={storiesCount}
-        isPaused={isPaused || isAdvertisementInfoOpen}
-        onComplete={handleProgressComplete}
-      />
-
-      <div className={styles.storyAd__topBar}>
-        <div className={styles.storyAd__topBar__left}>
-          <div className={styles.storyAd__topBar__avatar}>
-            <Avatar defaultImage={<Broadcast />} avatarUrl={avatarUrl} />
-          </div>
-          <div className={styles.storyAd__topBar__text}>
-            <Typography.BodyBold className={styles.storyAd__topBar__advertiserName}>
-              {ad.advertiser?.name}
-            </Typography.BodyBold>
-            <AdsBadge />
-          </div>
-        </div>
-        <div className={styles.storyAd__topBar__right}>
-          {isPaused ? (
-            <PlayIcon
-              className={styles.playStoryButton}
-              data-qa-anchor="play_button"
-              onClick={play}
-            />
-          ) : (
-            <PauseIcon
-              className={styles.pauseStoryButton}
-              data-qa-anchor="pause_button"
-              onClick={pause}
-            />
-          )}
-          <CloseButton
-            defaultClassName={clsx(styles.storyAd__closeButton)}
-            pageId={pageId}
-            onPress={onClose}
-          />
-        </div>
-      </div>
-
-      <div className={clsx(styles.storyImageContainer)}>
-        <img
-          ref={imageRef}
-          className={clsx(styles.storyImage)}
-          data-qa-anchor="image_view"
-          src={adImageUrl}
-          onLoad={imageLoaded}
-          alt="Story Image"
-          crossOrigin="anonymous"
-        />
-      </div>
-
-      {!loaded && <div className={styles.loadingOverlay}>{loader || <div>loading...</div>}</div>}
-
-      {ad.callToActionUrl && (
-        <div className={styles.hyperLinkContainer}>
-          <a
-            className={styles.hyperLink__text}
-            href={ad.callToActionUrl}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {ad.callToAction}
-          </a>
-        </div>
-      )}
-      <Button className={styles.infoIcon__button} onPress={() => openAdvertisementInfo()}>
-        <InfoCircle className={styles.infoIcon} />
-      </Button>
-      <StoryAdInformation
-        ad={ad}
-        isOpen={isAdvertisementInfoOpen}
-        targetRef={targetRef}
-        onOpenChange={(open) => !open && closeAdvertisementInfo()}
-      />
-    </div>
+    <UIStoryAd
+      pageId={pageId}
+      ad={ad}
+      adImageUrl={adImageUrl}
+      avatarUrl={avatarUrl}
+      currentIndex={currentIndex}
+      isPaused={isPaused}
+      storiesCount={storiesCount}
+      renderLoader={() => loader}
+      onComplete={handleProgressComplete}
+      onPauseClick={pause}
+      onPlayClick={play}
+      onClose={onClose}
+      onImageLoaded={onImageLoaded}
+      onAdvertisementInfoOpen={onAdvertisementInfoOpen}
+      onAdvertisementInfoClose={onAdvertisementInfoClose}
+    />
   );
 };
 
@@ -181,6 +98,6 @@ export const tester: Tester = (story) => {
 };
 
 export default {
-  renderer,
+  renderer: renderer,
   tester,
 };
