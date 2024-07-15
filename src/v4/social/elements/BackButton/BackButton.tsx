281921e2a68636b746c20cdef7533b985fa784b8
Bonn <pittawat@amity.co>
2024-07-15 18:35:58 +0700

    fix: split into RoundedBackButton and BackButton (#543)

--- src/v4/social/elements/BackButton/BackButton.tsx
+++ src/v4/social/elements/BackButton/BackButton.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { IconComponent } from '~/v4/core/IconComponent';
 import { useAmityElement } from '~/v4/core/hooks/uikit';
-import { Button, ButtonProps } from '~/v4/core/natives/Button';
-
+import { Button, ButtonProps } from '~/v4/core/natives/Button/Button';
 import styles from './BackButton.module.css';
 
 const BackButtonSvg = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
+    width="10"
+    height="17"
+    viewBox="0 0 10 17"
     fill="none"
-    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <circle cx="16" cy="16" r="16" fill="#000" fillOpacity="0.5"></circle>
-    <path
-      fill="currentColor"
-      d="M16.176 23.914c-.14.176-.422.176-.598 0L8.23 16.566a.405.405 0 010-.597l7.348-7.348c.176-.176.457-.176.598 0l.703.668c.176.176.176.457 0 .598l-5.45 5.449h12.024c.211 0 .422.21.422.422v.984c0 .246-.21.422-.422.422H11.43l5.449 5.484c.176.141.176.422 0 .598l-.703.668z"
-    ></path>
+    <path d="M8.62109 15.9141C8.44531 16.0898 8.19922 16.0898 8.02344 15.9141L0.640625 8.56641C0.5 8.39062 0.5 8.14453 0.640625 7.96875L8.02344 0.621094C8.19922 0.445312 8.44531 0.445312 8.62109 0.621094L9.32422 1.28906C9.46484 1.46484 9.46484 1.74609 9.32422 1.88672L2.96094 8.25L9.32422 14.6484C9.46484 14.7891 9.46484 15.0703 9.32422 15.2461L8.62109 15.9141Z" />
   </svg>
 );
 
@@ -48,9 +43,13 @@ export const BackButton = ({
   if (isExcluded) return null;
 
   return (
-    <Button style={themeStyles} className={styles.backButton} onPress={onPress}>
+    <Button
+      data-qa-anchor={accessibilityId}
+      className={styles.backButton}
+      onPress={onPress}
+      style={themeStyles}
+    >
       <IconComponent
-        data-qa-anchor={accessibilityId}
         defaultIcon={() => <BackButtonSvg className={defaultClassName} />}
         imgIcon={() => <img src={config.icon} alt={uiReference} className={imgClassName} />}
         defaultIconName={defaultConfig.icon}
