91bd7531a690f397c2e5fabbf7dfe45a18792746
Bonn <pittawat@amity.co>
2024-07-15 15:24:35 +0700

    fix: fix Linkify (#539)

--- src/v4/social/internal-components/Linkify/Linkify.tsx
+++ src/v4/social/internal-components/Linkify/Linkify.tsx
@@ -7,11 +7,18 @@ type UiKitLinkifyProps = Omit<React.ComponentProps<typeof Linkify>, 'componentDe
 
 export const UiKitLinkify = (props: UiKitLinkifyProps) => (
   <Linkify
+    {...props}
     options={{
       render: ({ attributes, content }) => {
-        const { href, ...props } = attributes;
+        const { href, ...attrs } = attributes;
         return (
-          <a className={styles.link} target="blank" rel="noopener noreferrer" href={href}>
+          <a
+            className={styles.link}
+            target="blank"
+            rel="noopener noreferrer"
+            href={href}
+            {...attrs}
+          >
             {content}
           </a>
         );
