b1f0376f3e41bb6c9ba19703286f06d5ee06c6b7
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-03-26 11:22:14 +0700

    feat: ASC-29235 - live stream post title bold (#996)
    * feat: ASC-28759 - view live stream post (#978)
    
    * feat: live stream view
    
    * fix: build error
    
    * feat: terminated live stream
    
    * feat: live stream terminated and deleted pages
    
    * feat: pendingpostcontent
    
    * feat: code review and customization
    
    * feat: hide edit post and preview link in live stream post (#980)
    
    * fix: video play icon
    
    * feat: livestream and poll posts in newsfeeds (#981)
    
    * feat: live stream post text content (#983)
    
    * fix: hasMore in load comment (#985)
    
    * fix: ASC-29059 - fail to report unreport (#984)
    
    * feat: fail to report and unreport due to no internect connection
    
    * fix: terminated state
    
    * fix: change error toast to info toast
    
    * feat: upcoming live (#986)
    
    * fix: apply concurrency on staging pipeline
    
    * fix: ASC-29053 - space between see more button (#988)
    
    * feat: live stream post text content
    
    * feat: space between see more
    
    * feat: ASC-29046 - live stream video custom control (#989)
    
    * feat: live stream video custom control
    
    * feat: vh to dvh for safari
    
    * fix: safari orientation
    
    * feat: popover style (#990)
    
    * feat: flashing seemore
    
    * fix: camealCase
    
    * fix: setStreamPlayer as dependency
    
    * fix: timer type
    
    * fix: empty description
    
    * fix: align tech specs with LiveStreamPlayerPage
    
    * feat: LiveStreamTitle Bold
    
    * fix: remove unnecessary css
    
    ---------
    
    Co-authored-by: phongnarin <phongnarin@social.plus>

--- src/v4/social/components/PostContent/TextContent/TextContent.tsx
+++ src/v4/social/components/PostContent/TextContent/TextContent.tsx
@@ -1,61 +1,52 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import * as linkify from 'linkifyjs';
+import { PostContentType } from '@amityco/ts-sdk';
+import useStream from '~/v4/social/hooks/useStream';
+import usePost from '~/v4/core/hooks/objects/usePost';
 import { Mentioned, Mentionees } from '~/v4/helpers/utils';
 import { LinkPreview } from '~/v4/social/components/PostContent/LinkPreview/LinkPreview';
-import styles from './TextContent.module.css';
-import * as linkify from 'linkifyjs';
 import { TextWithMention } from '~/v4/social/internal-components/TextWithMention/TextWithMention';
-import usePost from '~/v4/core/hooks/objects/usePost';
-import useStream from '~/v4/social/hooks/useStream';
 
-interface MentionHighlightTagProps {
-  children: ReactNode;
-  mentionee: Mentioned;
-}
-
-const MentionHighlightTag = ({ children }: MentionHighlightTagProps) => {
-  return <span className={styles.mentionHighlightTag}>{children}</span>;
-};
-
-const MAX_TEXT_LENGTH = 500;
-
-interface TextContentProps {
-  pageId?: string;
-  componentId?: string;
+type TextContentProps = {
   text?: string;
+  pageId?: string;
+  post?: Amity.Post;
+  componentId?: string;
   mentioned?: Mentioned[];
   mentionees?: Mentionees;
-  post?: Amity.Post;
-}
+};
 
 export const TextContent = ({
+  post,
+  text = '',
+  mentioned,
   pageId = '*',
   componentId = '*',
-  text = '',
   mentionees = [],
-  mentioned,
-  post,
 }: TextContentProps) => {
-  if (!text) {
-    return null;
-  }
+  if (!text) return null;
 
   const { post: childPost } = usePost(post?.children?.[0]);
 
   const linksFounded = linkify.find(text).filter((link) => link.type === 'url');
 
-  const isHasMedia =
-    childPost?.dataType === 'image' ||
-    childPost?.dataType === 'video' ||
-    childPost?.dataType === 'poll' ||
-    childPost?.dataType === 'liveStream';
+  const isHasMedia = [
+    PostContentType.IMAGE,
+    PostContentType.VIDEO,
+    PostContentType.POLL,
+    PostContentType.LIVESTREAM,
+  ].includes(childPost?.dataType);
+
+  const canPreviewShown = linksFounded && linksFounded.length > 0 && !isHasMedia;
 
   const stream = useStream(childPost?.data?.streamId);
 
   return (
     <>
-      {childPost?.dataType === 'liveStream' ? (
+      {childPost?.dataType === PostContentType.LIVESTREAM ? (
         <>
           <TextWithMention
+            isBold
             pageId={pageId}
             mentionees={mentionees}
             metadata={{ mentioned }}
@@ -81,7 +72,7 @@ export const TextContent = ({
           metadata={{ mentioned }}
         />
       )}
-      {linksFounded && linksFounded.length > 0 && !isHasMedia && (
+      {canPreviewShown && (
         <LinkPreview pageId={pageId} componentId={componentId} url={linksFounded[0].href} />
       )}
     </>
