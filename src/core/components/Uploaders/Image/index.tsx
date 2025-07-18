34632d4902dd4330af6ded9ff737ad07f59a1ebd
Pitchaya T <33589608+ptchayap@users.noreply.github.com>
2025-07-18 17:31:42 +0700

    Release/v3.13.2 (#119)
    * Release/v3.11.7 (#959)
    
    * fix: update displayName v3
    
    * chore(release): 3.11.7
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * Release/v3.11.8 (#960)
    
    * fix: add refresh to liveObject
    
    * fix: add refresh to liveObject
    
    * chore(release): 3.11.8
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * feat: bump sdk v7 (#977)
    
    * feat: bump sdk v7
    
    * chore(release): 3.12.0
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * Release/v3.13.0 (#1025)
    
    * feat: ASC-29534 - legacy unreadCount (#1016)
    
    * feat: applied sdk and use legacy unreadCount
    
    * fix: remove comment
    
    * feat: upgrade sdk version
    
    * feat: upgrade sdk version
    
    * feat: upgrade SDK
    
    * chore: update SDK package
    
    * chore(release): 3.13.0
    
    ---------
    
    Co-authored-by: Pitchaya T. <33589608+ptchayap@users.noreply.github.com>
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * Release/v3.13.1 (#1050)
    
    * fix: pending post button (#1040)
    
    * chore: bump sdk version
    
    * chore(release): 3.13.1
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * fix: show thumbnail for video right away after create (#1168)
    
    * chore(release): 3.13.2
    
    * chore(release): 3.13.2
    
    ---------
    
    Co-authored-by: ChayanitBm <chayanit@amity.co>
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    Co-authored-by: hwp <70026748+htutwaiphyoe@users.noreply.github.com>

--- src/core/components/Uploaders/Image/index.tsx
+++ src/core/components/Uploaders/Image/index.tsx
@@ -3,10 +3,10 @@ import LocalImage from './LocalImage';
 import DistantImage from './DistantImage';
 import { StyledImageProps } from './StyledImage';
 
-type ImageProps = { file?: File; fileId?: string } & StyledImageProps;
+type ImageProps = { file?: File; fileId?: string; fileUrl?: string } & StyledImageProps;
 
 export default function Image(props: ImageProps) {
-  if ('fileId' in props) return <DistantImage {...props} />;
-  if ('file' in props) return <LocalImage {...props} />;
+  if ('fileId' in props && !!props.fileId) return <DistantImage {...props} />;
+  if ('file' in props || !!props.fileUrl) return <LocalImage {...props} />;
   return null;
 }
