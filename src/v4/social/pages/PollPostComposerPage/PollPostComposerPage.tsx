017ec3b331d139ec70634ebfaae0b01f7f2c5bea
ChayanitBm <chayanit@amity.co>
2025-07-21 16:42:53 +0700

    fix: remove required targetId (#1173)

--- src/v4/social/pages/PollPostComposerPage/PollPostComposerPage.tsx
+++ src/v4/social/pages/PollPostComposerPage/PollPostComposerPage.tsx
@@ -224,10 +224,8 @@ export const PollPostComposerPage = ({ targetId, targetType }: PollPostComposerP
       return;
     }
 
-    if (!targetId) return;
-
     const createPostParams: Parameters<typeof PostRepository.createPost>[0] = {
-      targetId: targetId,
+      targetId: targetId!,
       targetType: targetType,
       data: { pollId, text: textValue.text },
       dataType: 'poll',
