a6e15e921073ca44db646b21a62889ce109b57e8
Pitchaya T <33589608+ptchayap@users.noreply.github.com>
2025-12-19 10:59:31 +0700

    fix: SP-33097 - add missing toast on back to viewer (#1373)
    * fix: add missing toast on back to viewer
    
    * fix: add missing leaving overlay
    
    * fix: co-host is kicked-out state

--- src/v4/social/features/livestream/internal-components/LivestreamStage/StreamerStage/StreamerStage.tsx
+++ src/v4/social/features/livestream/internal-components/LivestreamStage/StreamerStage/StreamerStage.tsx
@@ -62,6 +62,7 @@ const Stage = ({
 
   const { currentUserId } = useSDK();
   const { confirm } = useConfirmContext();
+  const { success } = useNotifications();
   const { buttonProps } = useDisconnectButton({});
 
   // LiveKit device management (inside room context)
