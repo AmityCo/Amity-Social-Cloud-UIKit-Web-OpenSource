2cd4a2b3d19869278fed0eb34c8e4c48e8e24692
Pitchaya T. <33589608+ptchayap@users.noreply.github.com>
2026-02-16 10:06:44 +0700

    fix: to show moderator badge for all users (#1446)

--- src/v4/social/components/Comment/Comment.tsx
+++ src/v4/social/components/Comment/Comment.tsx
@@ -356,8 +356,7 @@ export const Comment = ({
               {isHost ? (
                 <EventHostBadge withLabel />
               ) : (
-                isModeratorUser &&
-                !isBrandUser && <ModeratorBadge pageId={pageId} componentId={componentId} />
+                isModeratorUser && <ModeratorBadge pageId={pageId} componentId={componentId} />
               )}
 
               <TextWithMention
