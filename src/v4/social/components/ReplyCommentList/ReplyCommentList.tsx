c173ea0ca14a30087e8ab88e9edce7dd9990ee8d
ChayanitBm <chayanit@amity.co>
2026-06-09 14:48:57 +0700

    fix: PDT-3251 - delete comment tag (#1691)
    * fix: revert remove comment
    
    * fix: comment line

--- src/v4/social/components/ReplyCommentList/ReplyCommentList.tsx
+++ src/v4/social/components/ReplyCommentList/ReplyCommentList.tsx
@@ -92,7 +92,7 @@ export const ReplyCommentList = ({
     pageSize: 5,
     sortBy: isL2List ? 'firstCreated' : 'lastCreated',
     shouldCall: shouldFetch,
-    includeDeleted: true,
+    includeDeleted: false,
   });
 
   const notification = useNotifications();
