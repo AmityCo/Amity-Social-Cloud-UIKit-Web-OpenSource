7ddb2130e9e7a421e25670ad933743b91aae337b
Pitchaya T. <33589608+ptchayap@users.noreply.github.com>
2026-07-16 15:19:00 +0700

    fix: PDT-0000 - svg icon camelcase attrs (#1774)
    Co-authored-by: Pitchaya Thipkham <pitchaya@social.plus>

--- src/v4/icons/ExitPictureInPicture.tsx
+++ src/v4/icons/ExitPictureInPicture.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const ExitPictureInPicture = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" {...props}>
-      <g fill="none" fill-rule="evenodd" opacity=".87">
+      <g fill="none" fillRule="evenodd" opacity=".87">
         <path
           d="M18 4H4v10h14V4zm4 12V1.98C22 .88 21.1 0 20 0H2C.9 0 0 .88 0 1.98V16c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2zm-2 .02H2V1.97h18v14.05z"
           fill="currentColor"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path d="M-1-3h24v24H-1z" />
       </g>
--- src/v4/icons/Forward15.tsx
+++ src/v4/icons/Forward15.tsx
@@ -13,23 +13,23 @@ export const Forward15 = (props: React.SVGProps<SVGSVGElement>) => {
       <path
         d="M11 19V12L9 13.3125"
         stroke="white"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14 18.5627C14.3134 18.8373 14.7029 19 15.125 19C16.1605 19 17 18.0206 17 16.8125C17 15.6044 16.1605 14.625 15.125 14.625C14.7029 14.625 14.3134 14.7877 14 15.0623L14.375 12H17"
         stroke="white"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M21.1839 6.80412C26.0309 11.1977 26.2961 18.579 21.7762 23.2905C17.2563 28.0021 9.66302 28.2599 4.81605 23.8663C-0.0309169 19.4726 -0.296081 12.0914 4.22379 7.37984C6.7947 4.69988 10.36 3.46089 13.8486 3.69923M12.1429 1L15.1992 3.83294L12.1429 6.87098"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
