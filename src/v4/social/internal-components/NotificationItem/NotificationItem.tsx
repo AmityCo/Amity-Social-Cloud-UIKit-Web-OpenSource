3eab61d94e351cd0accdf0b208abd2fcee51233c
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2026-07-17 18:09:54 +0700

    Revert "feat: noti create event (#1762)"
    This reverts commit a9e111ab2384de8bcba03987e7fa8b868595a053.

--- src/v4/social/internal-components/NotificationItem/NotificationItem.tsx
+++ src/v4/social/internal-components/NotificationItem/NotificationItem.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 import { FileRepository, notificationTray } from '@amityco/ts-sdk';
 import { useAmityComponent } from '~/v4/core/hooks/uikit';
-import useCommunity from '~/v4/core/hooks/collections/useCommunity';
 import { UserAvatar } from '~/v4/social/elements/UserAvatar';
-import { CommunityAvatar } from '~/v4/social/elements/CommunityAvatar';
 import { Typography } from '~/v4/core/components';
 import { Timestamp } from '~/v4/social/elements/Timestamp';
-import { EventTypeBadge } from '~/v4/social/features/events/EventHub/elements';
-import { formatEventStartDate, formatEventStartTime } from '~/v4/social/utils/timezone';
 import { highlightedText } from '~/v4/social/utils/highlightedText';
 import { Button } from '~/v4/core/natives/Button/Button';
 import { usePageBehavior } from '~/v4/core/providers/PageBehaviorProvider';
-const EVENT_TRAY_CATEGORIES = ['event_created', 'event_reminder', 'event_started'];
+const EVENT_TRAY_CATEGORIES = ['eventCreated', 'eventCancelled', 'eventUpdated', 'eventInvitation'];
 import eventThumbnail from '~/v4/social/assets/images/event-default-thumbnail.png';
 import styles from './NotificationItem.module.css';
 
@@ -88,56 +84,6 @@ export const NotificationItem = ({
     }
   };
 
-  const isEventCreated = item.trayItemCategory === 'event_created' && item.event != null;
-
-  const eventCommunityId =
-    isEventCreated && item.event?.originType === 'community' ? item.event?.originId : undefined;
-  const { community: eventCommunity } = useCommunity({
-    communityId: eventCommunityId,
-    shouldCall: !!eventCommunityId,
-  });
-
-  const renderLeadingAvatar = () => {
-    if (isEventCreated) {
-      return (
-        <CommunityAvatar
-          pageId={pageId}
-          componentId={componentId}
-          community={eventCommunity ?? item.event?.targetCommunity}
-          className={styles.notificationItem__avatar}
-        />
-      );
-    }
-
-    if (item.event) {
-      return (
-        <img
-          onError={() => setErrorImage(true)}
-          data-size="small"
-          className={styles.notificationItem__eventCover}
-          src={
-            errorImage
-              ? eventThumbnail
-              : item.event.coverImage
-                ? FileRepository.fileUrlWithSize(item.event.coverImage?.fileUrl, 'medium')
-                : eventThumbnail
-          }
-        />
-      );
-    }
-
-    return (
-      <UserAvatar
-        shouldRedirectToUserProfile={false}
-        onPressAvatar={onClickItem}
-        pageId={pageId}
-        componentId={componentId}
-        userData={item.actionType === 'user' ? undefined : item.users[0]}
-        className={styles.notificationItem__avatar}
-      />
-    );
-  };
-
   return (
     <Button
       key={item._id}
@@ -148,26 +94,34 @@ export const NotificationItem = ({
     >
       <div className={styles.notificationItem}>
         <div className={styles.notificationItem__userInfo}>
-          {renderLeadingAvatar()}
-          <div className={styles.notificationItem__content}>
-            <Typography.Body className={styles.notificationItem__text}>
-              {highlightedText(item.templatedText, item.text)}
-            </Typography.Body>
-            {isEventCreated && (
-              <div
-                data-testid={`${accessibilityId}_event_notification_item`}
-                className={styles.notificationItem__eventContext}
-              >
-                {item.event?.type && <EventTypeBadge type={item.event.type} />}
-                <Typography.Caption className={styles.notificationItem__eventDateTime}>
-                  {formatEventStartDate(item.event!.startTime)}
-                </Typography.Caption>
-                <Typography.Caption className={styles.notificationItem__eventDateTime}>
-                  {formatEventStartTime(item.event!.startTime)}
-                </Typography.Caption>
-              </div>
-            )}
-          </div>
+          {item.event ? (
+            <img
+              onError={() => setErrorImage(true)}
+              data-size="small"
+              className={styles.notificationItem__eventCover}
+              src={
+                errorImage
+                  ? eventThumbnail
+                  : item.event.coverImage
+                    ? FileRepository.fileUrlWithSize(item.event.coverImage?.fileUrl, 'medium')
+                    : eventThumbnail
+              }
+            />
+          ) : (
+            <>
+              <UserAvatar
+                shouldRedirectToUserProfile={false}
+                onPressAvatar={onClickItem}
+                pageId={pageId}
+                componentId={componentId}
+                userData={item.actionType === 'user' ? undefined : item.users[0]}
+                className={styles.notificationItem__avatar}
+              />
+            </>
+          )}
+          <Typography.Body className={styles.notificationItem__text}>
+            {highlightedText(item.templatedText, item.text)}
+          </Typography.Body>
         </div>
         <Timestamp pageId={pageId} timestamp={item.lastOccurredAt} />
       </div>
