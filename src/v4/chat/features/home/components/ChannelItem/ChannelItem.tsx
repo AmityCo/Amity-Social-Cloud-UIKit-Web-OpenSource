63f0cc69d6dcf5153978a960d96d9aa835392171
ChayanitBm <chayanit@amity.co>
2026-07-22 15:52:53 +0700

    fix: laoding archive (#1801)
    Co-authored-by: Baimon <chayanit@social.plus>

--- src/v4/chat/features/home/components/ChannelItem/ChannelItem.tsx
+++ src/v4/chat/features/home/components/ChannelItem/ChannelItem.tsx
@@ -229,7 +229,10 @@ function ChannelItemSkeleton() {
   return (
     <div className={styles.channelItem__skeletonRow}>
       <Skeleton.Circle width="2.5rem" height="2.5rem" />
-      <Skeleton.Line width="8.75rem" height="0.625rem" />
+      <div className={styles.channelItem__skeletonLines}>
+        <Skeleton.Line width="8.75rem" height="0.625rem" />
+        <Skeleton.Line width="12.5rem" height="0.625rem" />
+      </div>
     </div>
   );
 }
